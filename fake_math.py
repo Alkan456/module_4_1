476041daa19127b49fc0ebad48dab1530564f978
Alexander_456 <shustrik43@gmail.com>
2024-10-01 06:27:47 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,9 @@
+
+def divde(first, second):
+   if second != 0:
+       f = first/second
+       return f
+   else:
+       return 'Ошибка'
+
+
---
+++ module_4_1.py
@@ -0,0 +1,13 @@
+
+from fake_math import divde as fake_div
+
+from true_math import devide as true_div
+
+result1 = fake_div(3, 0)
+result2 = true_div(48, 0)
+result3 = fake_div(4, 10)
+result4 = true_div(15, 17)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
